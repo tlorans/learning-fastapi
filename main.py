154edb90725f5c197262c66b7f613068929ef25a
tlorans <thomaslorans@gmail.com>
2024-07-14 07:33:35 +0200

    changed

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
 
 app = FastAPI()
 
+students = {
+    1: {
+        "name": "John",
+        "age": 17,
+        "class": "year 12"
+    }
+}
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def index():
+    return {"message": "Hello, World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/get-student/{student_id}")
+def get_students(student_id: int = Path(description="The ID of the student you want to view")):
+    return students.get(student_id)
