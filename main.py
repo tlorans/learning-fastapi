c945c6a3604709762c61c13fb4dab437079b573e
tlorans <thomaslorans@gmail.com>
2024-07-14 08:01:50 +0200

    test with stocks

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
+import random
 
 app = FastAPI()
 
-students = {
-    1: {
-        "name": "John",
-        "age": 17,
-        "class": "year 12"
+# Simulate a fake database with a thousand stocks
+stocks = [
+    {
+        "ticker": f"STOCK{i:04d}",
+        "name": f"Stock {i}",
+        "investment_zone": random.choice(["Zone A", "Zone B", "Zone C", "Zone D"]),
+        "price": round(random.uniform(10, 1000), 2)
     }
-}
+    for i in range(1, 1001)
+]
 
 @app.get("/")
 def index():
     return {"message": "Hello, World"}
 
-@app.get("/get-student/{student_id}")
-def get_students(student_id: int = Path(description="The ID of the student you want to view")):
-    return students.get(student_id)
+
+@app.get("/stocks/")
+def get_stocks(investment_zone: str = Query(None, description="The investment zone of the stocks you want to view")):
+    if investment_zone:
+        filtered_stocks = [stock for stock in stocks if stock["investment_zone"] == investment_zone]
+        return filtered_stocks
+    return stocks
+
