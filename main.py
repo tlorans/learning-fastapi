ae0d074642ed41611dedbe2b94187e517d375f40
tlorans <thomaslorans@gmail.com>
2024-07-14 09:24:54 +0200

    dependency to mypackagedata

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Path
 import json
 import os
+import my_data_package
 
 app = FastAPI()
 
@@ -22,8 +23,7 @@ def index():
 @app.get("/investment_zone/{zone}")
 def get_stocks_by_zone(zone: str = Path(description="The investment zone of the stocks you want to view")):
     # Load zone mapping from JSON file
-    with open("zone_mapping.json", "r") as f:
-        zone_mapping = json.load(f)
+    zone_mapping = my_data_package.get_zone_mapping()
     
     if zone not in zone_mapping:
         return {"message": "Zone not found"}
@@ -32,24 +32,19 @@ def get_stocks_by_zone(zone: str = Path(description="The investment zone of the 
     combined_stocks = []
 
     for constituent_zone in constituent_zones:
-        file_path = f"{constituent_zone}.json"
-        if os.path.exists(file_path):
-            with open(file_path, "r") as f:
-                combined_stocks.extend(json.load(f))
+        combined_stocks.extend(my_data_package.load_universe(constituent_zone))
     
     return combined_stocks
 
 @app.get("/returns/{date}")
 def get_returns(date:str = Path(description="The date for which you want to view returns")):
-    file_path = f"returns/{date}.json"
-    if os.path.exists(file_path):
-        with open(file_path, "r") as f:
-            return json.load(f)
-    else:
+    returns_data = my_data_package.load_returns(date)
+    if not returns_data:
         return {"message": "No returns available for the specified date"}
+    return returns_data
     
 
-@app.get("/returns/{zone}/{date}")
+@app.get("/zone_returns/{zone}/{date}")
 def get_zone_returns(zone: str = Path(description="The investment zone of the stocks you want to view"), 
                      date: str = Path(description="The date for which you want to view returns")):
     if zone not in zone_mapping:
@@ -59,19 +54,12 @@ def get_zone_returns(zone: str = Path(description="The investment zone of the st
     combined_stocks = []
 
     for constituent_zone in constituent_zones:
-        file_path = f"universe/{constituent_zone}.json"
-        if os.path.exists(file_path):
-            with open(file_path, "r") as f:
-                combined_stocks.extend(json.load(f))
+        combined_stocks.extend(my_data_package.load_universe(constituent_zone))
     
-    # Fetch the returns for these stocks
-    file_path = f"returns/{date}.json"
-    if not os.path.exists(file_path):
+    returns_data = my_data_package.load_returns(date)
+    if not returns_data:
         return {"message": "No returns available for the specified date"}
-    
-    with open(file_path, "r") as f:
-        all_returns = json.load(f)
-    
-    zone_returns = {stock: all_returns.get(stock, None) for stock in combined_stocks}
+
+    zone_returns = {stock: returns_data.get(stock, None) for stock in combined_stocks}
     
     return zone_returns
